40aa97a253b008d3a82003e4f559c8df7ca2147f
jamesrothmann <35830265+jamesrothmann@users.noreply.github.com>
2023-11-21 14:33:21 +0200

    Update folder.py

--- folder.py
+++ folder.py
@@ -40,7 +40,7 @@ def openaiapi(input_text, prompt_text):
                 frequency_penalty=0,
                 presence_penalty=0
             )
-            return response['choices'][0]['message']['content']
+            return response.choices[0].message.content
         except requests.exceptions.RequestException as e:
             print(f"Request failed (attempt {attempt + 1}/{MAX_RETRIES}): {e}")
             if attempt < MAX_RETRIES - 1:
